b9f2dc6ef6ecc7de565bbdf5b3a283c2160c001c
Farzaneh Fallahpour <farzanehhh.fl@gmail.com>
2021-11-15 15:15:18 +0330

    handel error

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const { request, response } = require("express");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -13,53 +12,50 @@ let users = [];
 app.post("/signin/users", (request, response) => {
   for (let i = 0; i < users.length; i++) {
     if (
-      users[i].Username === request.body.username &&
+      users[i].username === request.body.username &&
       users[i].password === request.body.password
     ) {
-      response.send({
+      response.send([{
         status: "you signed up befor, welcome to home page",
         data: request.body,
         enter: "Ok",
-      });
+        usersData:users[i],
+      }]);
+      return;
     }
   }
 
-  response.status(400).send({
+  response.status(400).send([{
     status: "you did not sign up before pls go to regestered page ",
     data: request.body,
     params: request.query,
-  });
+    usersData:users[i],
+  }]);
 });
 
 app.post("/signup/users", (request, response) => {
-  if (users.length !== 0) {
     for (let i = 0; i < users.length; i++) {
       if (
         users[i].username === request.body.username &&
         users[i].password === request.body.password
       ) {
-        response.status(400).send({
-          status: "you did not sign up bedor pls go to regestered page ",
+        response.status(400).send([{
+          status: "you did not sign up before pls go to regestered page ",
           data: request.body,
           params: request.query,
-        });
-      } else {
-        users.push(request.body);
-        response.send({
-          status: "you sign up,welcom to home page",
-          data: request.body,
-          enter: "ok",
-        });
+          usersData:users[i],
+        }]);
+        return;
       }
     }
-  } else {
+
     users.push(request.body);
-    response.send({
-      status: "you sign up,go to home page",
+    response.send([{
+      status: "you sign up,welcom to home page",
       data: request.body,
       enter: "ok",
-    });
-  }
+      usersData:users[i],
+    }]);
   console.log(users);
 });
 
